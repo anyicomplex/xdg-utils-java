4fc1ffe13167c91cd23e766e53467202ce5b198a
安怡 <anyicomplex@protonmail.com>
2022-02-13 07:43:51 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# xdg-utils-java
-[xdg-utils](https://www.freedesktop.org/wiki/Software/xdg-utils/) wrapper for Java, only working on Linux.
+# xdg-utils-java [![Java CI with Gradle](https://github.com/anyicomplex/xdg-utils-java/actions/workflows/gradle.yml/badge.svg)](https://github.com/anyicomplex/xdg-utils-java/actions/workflows/gradle.yml) [![Publish to Maven Central](https://github.com/anyicomplex/xdg-utils-java/actions/workflows/gradle-publish.yml/badge.svg)](https://github.com/anyicomplex/xdg-utils-java/actions/workflows/gradle-publish.yml) ![License](https://img.shields.io/github/license/anyicomplex/xdg-utils-java)
+**NOTE: This is NOT an implementation!** [xdg-utils](https://www.freedesktop.org/wiki/Software/xdg-utils/) wrapper for Java, only working on Linux.
+
+## Current State
+| Supported libGDX feature | Version |  State   |
+|:------------------------:|:-------:|:--------:|
+|     xdg-desktop-icon     |  1.1.3  | Complete |
+|     xdg-desktop-menu     |  1.1.3  | Complete |
+|        xdg-email         |  1.1.3  | Complete |
+|    xdg-icon-resource     |  1.1.3  | Complete |
+|         xdg-mime         |  1.1.3  | Complete |
+|         xdg-open         |  1.1.3  | Complete |
+|     xdg-screensaver      |  1.1.3  | Complete |
+|       xdg-settings       |  1.1.3  | Complete |
+
+## Add this repo to your project dependency
+Step 1. Add the Maven Central repository to your build file
+```groovy
+allprojects {
+	repositories {
+		...
+		mavenCentral()
+	}
+}
+```
+
+Step 2. Add the dependency
+```groovy
+dependencies {
+    implementation 'io.github.anyicomplex:xdg-utils-java:1.1.3b1'
+}
+```
+
+## System properties
+`com.anyicomplex.xdg.utils.scriptPath`: the specific path to load script.
---
+++ src/main/java/com/anyicomplex/xdg/utils/XDGIconResource.java
@@ -0,0 +1,79 @@
+package com.anyicomplex.xdg.utils;
+
+import java.util.ArrayList;
+
+import static com.anyicomplex.xdg.utils.XDGIconResource.Option.*;
+import static com.anyicomplex.xdg.utils.XDGIconResource.Subcommand.*;
+import static com.anyicomplex.xdg.utils.XDGUtils.isEmpty;
+import static com.anyicomplex.xdg.utils.XDGUtils.notNullBoolean;
+
+public class XDGIconResource {
+
+    private XDGIconResource(){}
+
+    public static final String FILE_NAME = "xdg-icon-resource";
+
+    public static final class Subcommand {
+        private Subcommand(){}
+        public static final String INSTALL = "install";
+        public static final String UNINSTALL = "uninstall";
+        public static final String FORCEUPDATE = "forceupdate";
+    }
+
+    public static final class Option {
+        private Option(){}
+        public static final String NOUPDATE = "--noupdate";
+        public static final String NOVENDOR = "--novendor";
+        public static final String THEME = "--theme";
+        public static final String CONTEXT = "--context";
+        public static final String MODE = "--mode";
+        public static final String SIZE = "--size";
+    }
+
+    public static String getScriptPath() {
+        return XDGUtils.getScriptFile(FILE_NAME).getAbsolutePath();
+    }
+
+    public static String install(int[] exitCode, Boolean noupdate, Boolean novendor,
+                                 String theme, String context, String mode, int size, String iconFile, String iconName) {
+        ArrayList<String> args = new ArrayList<>(14);
+        args.add(getScriptPath());
+        args.add(INSTALL);
+        args.add(notNullBoolean(noupdate) ? NOUPDATE : "");
+        args.add(notNullBoolean(novendor) ? NOVENDOR : "");
+        args.add(isEmpty(theme) ? "" : THEME);
+        args.add(isEmpty(theme) ? "" : theme);
+        args.add(isEmpty(context) ? "" : CONTEXT);
+        args.add(isEmpty(context) ? "" : context);
+        args.add(isEmpty(mode) ? "" : MODE);
+        args.add(isEmpty(mode) ? "" : mode);
+        args.add(SIZE);
+        args.add(Integer.toString(size));
+        args.add(iconFile);
+        args.add(iconName);
+        return XDGUtils.process(exitCode, args);
+    }
+
+    public static String uninstall(int[] exitCode, Boolean noupdate, String theme, String context, String mode, int size, String iconName) {
+        ArrayList<String> args = new ArrayList<>(12);
+        args.add(getScriptPath());
+        args.add(UNINSTALL);
+        args.add(notNullBoolean(noupdate) ? NOUPDATE : "");
+        args.add(isEmpty(theme) ? "" : THEME);
+        args.add(isEmpty(theme) ? "" : theme);
+        args.add(isEmpty(context) ? "" : CONTEXT);
+        args.add(isEmpty(context) ? "" : context);
+        args.add(isEmpty(mode) ? "" : MODE);
+        args.add(isEmpty(mode) ? "" : mode);
+        args.add(SIZE);
+        args.add(Integer.toString(size));
+        args.add(iconName);
+        return XDGUtils.process(exitCode, args);
+    }
+
+    public static String forceupdate(int[] exitCode, String theme, String mode) {
+        return XDGUtils.process(exitCode, getScriptPath(), FORCEUPDATE, isEmpty(theme) ? "" : THEME, isEmpty(theme) ? "" : theme,
+                isEmpty(mode) ? "" : MODE, isEmpty(mode) ? "" : mode);
+    }
+
+}
